ed54d85355971a033b06ec5c89f2adab60ab9652
gimdaeyeon <rlaeodus1306@naver.com>
2024-12-03 23:48:09 +0900

    Init: 프로젝트 초기 생성

---
+++ src/main/java/com/app/contbase/ContTbaseApplication.java
@@ -0,0 +1,13 @@
+package com.app.contbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContTbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContTbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/contbase/ContTbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.contbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContTbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
